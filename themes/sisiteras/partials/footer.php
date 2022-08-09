b29b837ed636146616cbc78787fbbec799a44181
Desa Garut <desagarut.net@gmail.com>
2022-08-09 18:55:41 +0700

    Update footer.php

--- themes/sisiteras/partials/footer.php
+++ themes/sisiteras/partials/footer.php
@@ -6,9 +6,9 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h5 class="text-light mb-4">Address</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><?= $kabupaten['alamat_kantor_kecamatan']; ?></p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><?= $kabupaten['telepon_kecamatan']; ?></p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i><?= $kabupaten['email_kecamatan']; ?></p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><?= $kabupaten['alamat_kantor']; ?></p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><?= $kabupaten['telepon']; ?></p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i><?= $kabupaten['email']; ?></p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a>

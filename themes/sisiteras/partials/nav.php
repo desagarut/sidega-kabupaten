d5be674a0fffb423635a337830006e29f88ccd80
Desa Garut <desagarut.net@gmail.com>
2022-08-09 18:24:16 +0700

    Update nav.php

--- themes/sisiteras/partials/nav.php
+++ themes/sisiteras/partials/nav.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-lg bg-danger navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s"> <a href="#" class="navbar-brand ms-3 d-lg-none">MENU</a>
   <button type="button" class="navbar-toggler me-3" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"> <span class="navbar-toggler-icon"></span> </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
-  <div class="navbar-nav me-auto p-3 p-lg-0"> <a href="<?= site_url('first') ?>" class="nav-item nav-link " aria-label="Toggle navigation">Home</a> <a href="<?= site_url('first') ?>" class="nav-item nav-link " aria-label="Toggle navigation">Pengembang</a>
+  <div class="navbar-nav me-auto p-3 p-lg-0"> <a href="<?= site_url('first') ?>" class="nav-item nav-link " aria-label="Toggle navigation">Home</a>
     <?php if(menu_atas) : ?>
     <?php foreach($menu_atas as $menu) : ?>
     <div class="nav-item dropdown">

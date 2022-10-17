145685a75d4082e9f1c226b27d5a2106fc516d73
Desa Garut <desagarut.net@gmail.com>
2022-10-17 13:45:53 +0700

    Update vendor.php

--- themes/desagarut/partials/vendor.php
+++ themes/desagarut/partials/vendor.php
@@ -1,21 +1,45 @@
-<?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <!-- Vendor Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="container py-5 mb-5">
-            <div class="bg-white">
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.3s">
+    <div class="container py-5 mb-5">
+        
+        <div class="bg-white">
+        <?php foreach ($sinergi_program as $key => $program) : ?>
+            <?php $baris[$program['baris']][$program['kolom']] = $program; ?>
+        <?php endforeach; ?>
+            
                 <div class="owl-carousel vendor-carousel">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-1.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-2.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-3.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-4.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-5.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-6.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-7.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-8.jpg")?>" alt="">
-                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/vendor-9.jpg")?>" alt="">
+                <?php foreach ($baris as $baris_program) : ?>
+                    <?php $width = 100 / count($baris_program) - count($baris_program) ?>
+                    <?php foreach ($baris_program as $key => $program) : ?>
+                        <img src="<?= base_url() . LOKASI_GAMBAR_WIDGET . $program['gambar'] ?>" alt="<?= $program['judul'] ?>">
+                    <?php endforeach; ?>
+                    <?php endforeach; ?>
                 </div>
-            </div>
+            
         </div>
     </div>
-    <!-- Vendor End -->
+</div>
+<!-- Vendor End -->
+<!-- ======= Clients Section ======= 
+    <section id="clients" class="clients">
+      <div class="container" data-aos="zoom-in">
+        <div class="clients-slider swiper-container">
+		<?php foreach ($sinergi_program as $key => $program) : ?>
+        <?php $baris[$program['baris']][$program['kolom']] = $program; ?>
+		<?php endforeach; ?>
+          <div class="swiper-wrapper align-items-center">
+			<?php foreach ($baris as $baris_program) : ?> 
+            <div class="swiper-slide" align="center">
+            <?php $width = 100 / count($baris_program) - count($baris_program) ?>
+			<?php foreach ($baris_program as $key => $program) : ?>
+            <a href="<?= $program['tautan'] ?>" target="_blank"><img src="<?= base_url() . LOKASI_GAMBAR_WIDGET . $program['gambar'] ?>" class="img-fluid" alt="<?= $program['judul'] ?>"></a>
+			<?php endforeach; ?>
+            </div>
+			<?php endforeach; ?>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </section><!-- End Clients Section -->

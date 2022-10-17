2855d595e2fe06a83adddafd13ffa6fbf041fe3e
Desa Garut <desagarut.net@gmail.com>
2022-10-17 11:33:52 +0700

    Update Tema Desa Garut
    Ganti Gambar Utama Carousel

--- themes/desagarut/partials/header-carousel.php
+++ themes/desagarut/partials/header-carousel.php
@@ -3,7 +3,7 @@
 <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="w-100" src="<?= base_url("$this->theme_folder/$this->theme/assets/img/carousel-2.jpg") ?>" alt="Image">
+            <img class="w-100" src="<?= base_url("$this->theme_folder/$this->theme/assets/img/sidega-001.jpg") ?>" alt="Image">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px;">
                     <h4 class="text-white text-uppercase mb-2 animated slideInDown">Selamat Datang di</h4>
